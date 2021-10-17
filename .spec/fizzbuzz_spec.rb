03111a7d0bb979e35eccaba8ec75f5a8a3924484
Caeser F Ibrahim <77758062+caeserlondon@users.noreply.github.com>
2021-10-18 00:19:37 +0100

    adding the lib and the spec

--- .spec/fizzbuzz_spec.rb
+++ .spec/fizzbuzz_spec.rb
@@ -1,6 +1,21 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz("3")).to eq 'fizz'
+  it 'returns "fizz" when passed multiple of 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed a number that is not multiple of 3 and 5' do
+    expect(fizzbuzz(4)).to eq 4
   end
 end
--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
-def fizzbuzz(number)
-  'fizz'
+def fizzbuzz(num)
+  output = ''
+  output += 'fizz' if num % 3 == 0
+  output += 'buzz' if num % 5 == 0
+  output = num if output == ''
+  output
 end
